afafdd9aab8e79e8b56c5b6125bebf2fbab48a7c
codefaux <codefaux@solacenet.org>
2026-01-08 12:18:22 -0800

    Initial commit, forked from codefaux/webhook-acquisition-initiator

---
+++ cfsonarrmatcher/__init__.py
@@ -0,0 +1,4 @@
+from .matcher import (build_token_frequencies, clean_sonarr_data, clean_text,
+                      compute_weighted_overlap, extract_episode_hint,
+                      match_title_to_sonarr_episode,
+                      match_title_to_sonarr_show, score_candidate)
